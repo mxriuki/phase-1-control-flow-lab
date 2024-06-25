898ad0ec4e2ab4d27604d1c6356df7333dc6471b
mxriuki <mwendav37@gmail.com>
2024-06-25 10:20:20 +0300

    controlFlow

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function scuberGreetingForFeet(distance){
+    // Write your code here!
+    //let distance
+    if (distance <= 400) {
+      return 'This one is on me!';
+    }
+    else if (distance >400 && distance<=2000) {
+      return 'That will be twenty bucks.';
+  }
+  
+  function ternaryCheckCity(){
+  else if (distance >2000 && distance<=2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if(distance>2500){
+    return 'No can do.';
+  }
+  }
+  function ternaryCheckCity(city){
+    // Write your code here!
+    //let city
+    if (city === 'NYC') {
+      return 'Ok, sounds good.';
+    }
+    else {
+      return 'No go.';
+    }
+  }
+  
+  function switchOnCharmFromTip(){
+  function switchOnCharmFromTip(tip){
+    // Write your code here!
+    //let tip
+    if (tip === 'generous') {
+      return 'Thank you so much.';
+    }
+    else if (tip === 'not as generous') {
+      return 'Thank you.';
+    }
+    else {
+      return 'Bye.';
+    }
+  }
